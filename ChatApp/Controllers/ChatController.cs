1e83b56c991a4ea24a26e41e0382ace999eee4e7
ETSTUR\kerimcan.islamoglu <kerimcan.islamoglu@etstur.com>
2021-08-31 16:40:07 +0300

    Add project files.

---
+++ ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,32 @@
+﻿using ChatApp.Hubs;
+using ChatApp.Hubs.Clients;
+using ChatApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub, IChatClient> _chatHub;
+
+        public ChatController(IHubContext<ChatHub, IChatClient> chatHub)
+        {
+            _chatHub = chatHub;
+        }
+
+        [HttpPost("messages")]
+        public async Task Post(ChatMessage message)
+        {
+            await _chatHub.Clients.All.ReceiveMessage(message);
+        }
+
+    }
+}
---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatApp.Hubs.Clients;
+using ChatApp.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub: Hub<IChatClient>
+    {
+        public async Task SendMessage(ChatMessage message)
+        {
+            await Clients.All.ReceiveMessage(message);
+        }
+    }
+}
